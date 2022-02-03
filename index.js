c33bffff3d7fde9517bee634e20f8c59232a361b
Jafr0 <zhek42@yandex.ru>
2022-02-03 22:23:43 +0300

    fix: almost done

--- index.js
+++ index.js
@@ -4,70 +4,119 @@ const playFrwrd = document.querySelector('.next-song');
 const playBck = document.querySelector('.previous-song');
 const logo = document.querySelector('.logo');
 const art = document.querySelector('.art');
-
-let playNum = 0;
+const artist = document.querySelector('.song-artist');
+const song = document.querySelector('.song-title');
+const player = document.querySelector(".container");
+let playtime = 0;
 let arr = [0, 1]
 
-let playPause = function () {
-
-}
 
 
-
-/* playBtn.addEventListener(
-	"click",
-	() => {
-		playBtn.classList.toggle('pause')
-
-		if (playBtn.classList.contains('pause')) {
-			playBtn.classList.add("pause");
-			audio.play();
-		} else {
-			playBtn.classList.remove("pause");
-			audio.pause();
-		}
-	},
-	false
-); */
 
 function toggleBtn() {
 	playBtn.classList.toggle('pause');
 	if (playBtn.classList.contains('pause')) {
 		playBtn.classList.add("pause");
 		audio.play();
+		logo.classList.add('scale')
 	} else {
 		playBtn.classList.remove("pause");
 		audio.pause();
+		logo.classList.remove('scale')
 	}
 
 }
 playBtn.addEventListener('click', toggleBtn);
 
+
 function playNext() {
-	playNum++
-	if (playNum > arr.length - 1) {
-		playNum = arr[0]
+	playtime++
+	if (playtime > arr.length - 1) {
+		playtime = arr[0]
+
 	}
-	audio.src = `assets/audio/${playNum}.mp3`
+	audio.src = `assets/audio/${playtime}.mp3`
 	audio.play()
 	playBtn.classList.add("pause")
-	logo.src = `assets/img/${playNum}.png`
-	art.src = `assets/img/${playNum}.png`
-}
+	logo.src = `assets/img/${playtime}.png`
+	art.src = `assets/img/${playtime}.png`
+	logo.classList.add('scale')
+	if (`assets/img/${playtime}.png` === `assets/img/1.png`) {
+		artist.textContent = "Beyonce"
+		song.textContent = "Don't Hurt Yourself"
+	}
+	else {
+		artist.textContent = "Dua Lipa"
+		song.textContent = "Don't Start Now"
+	}
 
+}
 playFrwrd.addEventListener('click', playNext)
 
 function playBack() {
-	playNum--
-	if (playNum < 0) {
-		playNum = arr.length - 1
+	playtime--
+	if (playtime < 0) {
+		playtime = arr.length - 1
+
 	}
-	audio.src = `assets/audio/${playNum}.mp3`
+	audio.src = `assets/audio/${playtime}.mp3`
 	audio.play()
 	playBtn.classList.add("pause")
-	logo.src = `assets/img/${playNum}.png`
-	art.src = `assets/img/${playNum}.png`
-}
+	logo.src = `assets/img/${playtime}.png`
+	art.src = `assets/img/${playtime}.png`
+	logo.classList.add('scale')
+	if (`assets/img/${playtime}.png` === `assets/img/1.png`) {
+		artist.textContent = "Beyonce"
+		song.textContent = "Don't Hurt Yourself"
+	}
+	else {
+		artist.textContent = "Dua Lipa"
+		song.textContent = "Don't Start Now"
+	}
 
+}
 playBck.addEventListener('click', playBack)
+
+
+audio.addEventListener(
+	"loadeddata",
+	() => {
+		player.querySelector(".time .length").textContent = timeCode(
+			audio.duration
+		);
+
+	},
+
+);
+
+const timeline = player.querySelector(".timeline");
+timeline.addEventListener("click", chunk => {
+	const widthTime = window.getComputedStyle(timeline).width;
+	const osttime = chunk.offsetX / parseInt(widthTime) * audio.duration;
+	audio.currentTime = osttime;
+});
+
+
+setInterval(() => {
+	const progressBar = player.querySelector(".progress");
+	progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+	player.querySelector(".time .current-time").textContent = timeCode(
+		audio.currentTime
+	);
+});
+
+function timeCode(time) {
+	let seconds = parseInt(time);
+	let minutes = parseInt(seconds / 60);
+	seconds -= minutes * 60;
+	const hours = parseInt(minutes / 60);
+	minutes -= hours * 60;
+
+	if (hours === 0) {
+		return `${minutes}:${String(seconds % 60).padStart(2, 0)}`
+	};
+	return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+		seconds % 60
+	).padStart(2, 0)}`;
+}
 
